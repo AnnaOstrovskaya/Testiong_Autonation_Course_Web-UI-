20151d43e31877bc1ab5848a73796b2d6f3408d7
AnnaOstrovskaya <85437758+AnnaOstrovskaya@users.noreply.github.com>
2022-03-14 12:06:12 +0000

    Add files via upload

---
+++ ui_tests/src/test/java/com/geekbrains/AppTest.java
@@ -0,0 +1,28 @@
+package com.geekbrains;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( false );
+    }
+
+    @Test
+    public void shouldAnswerWithTrue1()
+    {
+        System.out.println("Second test");
+    }
+
+
+}
